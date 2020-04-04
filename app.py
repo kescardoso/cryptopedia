01111036c17c320553f3485a522099adf8e70025
Kes Cardoso <kesia.development@gmail.com>
2020-04-04 23:08:55 +0000

    Install PyMongo and test first route

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+# Environment Variables
+from os import path
+if path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
 
 
+app.config["MONGO_NAME"] = 'thecryptopedia'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/get_terms')
+def get_terms():
+    return render_template("terms.html", terms=mongo.db.terms.find())
 
 
 if __name__ == '__main__':
