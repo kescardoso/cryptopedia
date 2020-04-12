9901459550dfb8f0efe6a08ff4282744b533131e
Kes Cardoso <kesia.development@gmail.com>
2020-04-12 12:36:39 +0000

    Add docstrings

--- app.py
+++ app.py
@@ -6,55 +6,58 @@ from bson.objectid import ObjectId
 from forms import RegistrationForm, LoginForm
 # Environment variables
 from os import path
-if path.exists("env.py"):
+if path.exists('env.py'):
     import env
 
 app = Flask(__name__)
 
 # MongoDB settings
-app.config["MONGO_NAME"] = 'cryptopedia'
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config['MONGO_NAME'] = 'cryptopedia'
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 # Flask settings
-app.config["SECRET_KEY"] = '!gMcT*jnqvez&'
+app.config['SECRET_KEY'] = '!gMcT*jnqvez&'
 
 mongo = PyMongo(app)
 
 
-# CRUD: bind and display list of all terms in the database:
+### CRUD FUNCTIONS:
+
+
 @app.route('/')
 @app.route('/get_terms')
 def get_terms():
-    return render_template("terms.html",
+    """ CRUD: bind and display a list of all terms in the database """
+    return render_template('terms.html',
                             terms=mongo.db.terms.find())
 
 
-# CRUD: get form to add new term:
 @app.route('/add_term')
 def add_term():
+    """ CRUD: get form to add new term """
     return render_template('addterm.html',
                            categories=mongo.db.categories.find())
 
 
-# CRUD: add new term to the database:
 @app.route('/insert_term', methods=['POST'])
 def insert_term():
+    """ CRUD: add new term to the database """
     terms = mongo.db.terms
     terms.insert_one(request.form.to_dict())
     return redirect(url_for('get_terms'))
 
 
-# CRUD: get form to edit term:
 @app.route('/edit_term/<term_id>')
 def edit_term(term_id):
-    the_term = mongo.db.terms.find_one({"_id": ObjectId(term_id)})
+    """ CRUD: get form to edit term """
+    the_term = mongo.db.terms.find_one({'_id': ObjectId(term_id)})
     all_categories = mongo.db.categories.find()
     return render_template('editterm.html', term=the_term,
                            categories=all_categories)
 
 
-# CRUD: edit term into the database:
-@app.route('/update_term/<term_id>', methods=["POST"])
+@app.route('/update_term/<term_id>', methods=['POST'])
 def update_term(term_id):
+    """ CRUD: edit term into the database """
     terms = mongo.db.terms
     terms.update({'_id': ObjectId(term_id)}, {
         'term_name': request.form.get('term_name'),
@@ -64,53 +67,53 @@ def update_term(term_id):
     return redirect(url_for('get_terms'))
 
 
-# CRUD: delete term from the database:
 @app.route('/delete_term/<term_id>')
 def delete_term(term_id):
+    """ CRUD: delete term from the database """
     mongo.db.terms.remove({'_id': ObjectId(term_id)})
     return redirect(url_for('get_terms'))
 
 
-# CRUD: bind and display list of categories from the database
 @app.route('/get_categories')
 def get_categories():
+    """ CRUD: bind and display list of categories from the database """
     return render_template('categories.html',
                            categories=mongo.db.categories.find())
 
 
-# CRUD: get form to edit category
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
+    """ CRUD: get form to edit category """
     return render_template('editcategory.html',
                            category=mongo.db.categories.find_one(
                             {'_id': ObjectId(category_id)}))
 
 
-# CRUD: edit category into the database
 @app.route('/update_category/<category_id>', methods=['POST'])
 def update_category(category_id):
+    """ CRUD: edit category into the database """
     mongo.db.categories.update(
         {'_id': ObjectId(category_id)},
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
 
 
-# CRUD: delete categories from the databse
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
+    """ CRUD: delete categories from the databse """
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
     return redirect(url_for('get_categories'))
 
 
-# CRUD: get form to add new category to the database
 @app.route('/add_category')
 def add_category():
+    """ CRUD: get form to add new category to the database """
     return render_template('addcategory.html')
 
 
-# CRUD: add new Category to the database
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
+    """ CRUD: add new Category to the database """
     category_doc = {'category_name': request.form.get('category_name')}
     mongo.db.categories.insert_one(category_doc)
     return redirect(url_for('get_categories'))
@@ -139,9 +142,12 @@ def about():
     return render_template("about.html")
 
 
-# User Registration
+### USER FORMS
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """ New user registration form with validators and alerts """
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Success! Account created for {form.username.data}.', 'badge light-green lighten-4')
@@ -149,9 +155,9 @@ def register():
     return render_template("register.html", title='Register', form=form)
 
 
-# User Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """ User login form with validators and alerts """
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
